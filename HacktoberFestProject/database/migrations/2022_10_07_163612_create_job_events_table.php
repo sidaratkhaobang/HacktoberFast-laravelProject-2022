0629a6d9d614d299eb1af5bed88990e4315b7cd5
sidarat khaobang <sidaratkhaobang@MacBook-Air-khxng-sidarat.local>
2022-10-07 23:41:01 +0700

    update db job event

--- HacktoberFestProject/database/migrations/2022_10_07_163612_create_job_events_table.php
+++ HacktoberFestProject/database/migrations/2022_10_07_163612_create_job_events_table.php
@@ -14,7 +14,7 @@ class CreateJobEventsTable extends Migration
     public function up()
     {
         Schema::create('job_events', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('code');
             $table->string('name');
             $table->text('description')->nullable();
